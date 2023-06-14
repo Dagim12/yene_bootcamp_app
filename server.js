f8a1587cb52db74bdf07ca7bb6de26275fc0867a
dagim12 <dbelayneh@excellerentsolutions.com>
2023-06-14 22:40:59 +0300

    mongoose installed and mongodb connected

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-//Load routes file
-const bootcamps = require("./routes/bootcamps");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDb = require("./config/db");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//connect to database
+connectDb();
+
+//Load routes file
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+//Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // mount routes
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  //close server and exit process
+  server.close(() => process.exit(1));
+});
